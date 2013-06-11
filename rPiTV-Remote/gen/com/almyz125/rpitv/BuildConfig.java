cc51041626ab520f648979b32f65f7ed72dc4eda
Alex Markessinis <alex@markea-11-t.SAGE.EDU>
2013-06-11 12:00:18 -0400

    Initial build

---
+++ rPiTV-Remote/gen/com/almyz125/rpitv/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.almyz125.rpitv;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ rPiTV-Remote/src/com/almyz125/rpitv/Remote.java
@@ -0,0 +1,41 @@
+package com.almyz125.rpitv;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+public class Remote extends Activity {
+	private WebView remote;
+	private String url;
+	private String uri;
+	final Activity activity = this;
+
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_remote);
+		remote = (WebView) this.findViewById(R.id.remoteView);
+		remote.getSettings().setJavaScriptEnabled(true);
+		remote.setWebViewClient(new WebViewClient() {
+			public void onReceivedError(WebView view, int errorCode,
+					String description, String failingUrl) {
+				Toast.makeText(activity,description + "\nCheck your IP and Port.",
+						Toast.LENGTH_LONG).show();
+			}
+		});
+		load();
+	}
+
+	public void load() {
+		url = "http://" + Information.url + ":" + Information.port + "/remote";
+		uri = Uri.parse(url).toString();
+		remote.loadUrl(uri);
+	}
+
+	@Override
+	public void onBackPressed() {
+		finish();
+	}
+}
